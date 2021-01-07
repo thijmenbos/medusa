d556d2b06c7c70121df28ef959d25e0c6fb51237
Thijmen Bos <thijmenbos1502@gmail.com>
2021-01-07 18:24:03 +0100

    Smooth Camera Movement

---
+++ medusa/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
