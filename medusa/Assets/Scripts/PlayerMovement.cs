820a83271a4e6111352bb7996a16de81f8c92e04
Thijmen Bos <thijmenbos1502@gmail.com>
2021-01-07 17:52:16 +0100

    Animations Jimmy (Left and Right still to be done)

--- medusa/Assets/Scripts/PlayerMovement.cs
+++ medusa/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,33 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 4f;
     public Rigidbody2D rb;
     Vector2 movement;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update() 
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        setAnimation();
     }
 
+    void setAnimation()
+    {
+        if (movement != Vector2.zero)
+        {
+            anim.SetFloat("MoveX", movement.x);
+            anim.SetFloat("MoveY", movement.y);
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+    }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
